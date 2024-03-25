fa92da371b909152c9cd1c602fae84a199937b19
Birinder <birindergill53@gmail.com>
2024-03-25 18:49:18 +0530

    production shit

--- app/Http/Controllers/WapiController.php
+++ app/Http/Controllers/WapiController.php
@@ -36,7 +36,7 @@ class WapiController extends Controller
             'to' => $data['to'],
             'counter' => $messageNumber + 1,
             "type" =>  $data['type'],
-            'messageText' => $data['type'] === 'media'
+            'messageText' => $data['type'] === 'image'
                              ? $this->generateAndStoreFile($from, $media['data'], $media['mimetype'])
                               : $message,
             'messageId' => $data['id']['id'],
