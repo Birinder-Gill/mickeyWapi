9ae0df9d9ca7dabdb81490a3b764d0a2af7afee2
Birinder <birindergill53@gmail.com>
2024-03-25 18:38:13 +0530

    production shit

--- app/Http/Controllers/WapiController.php
+++ app/Http/Controllers/WapiController.php
@@ -6,6 +6,7 @@ use App\Models\WapiMessage;
 use App\Services\WhatsAppApiService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class WapiController extends Controller
 {
@@ -26,28 +27,43 @@ class WapiController extends Controller
         $personName = $data['notifyName'];
         $from = $data['from'];
         $hash = $data['id']['_serialized'];
+        $media = request()->json()->all()['data']["media"];
 
-
-        $messageNumber = WapiMessage::where('from',$from)->count();
+        $messageNumber = WapiMessage::where('from', $from)->count();
         $logArray = [
             'from' => $from,
             'displayName' => $personName,
             'to' => $data['to'],
             'counter' => $messageNumber + 1,
             "type" =>  $data['type'],
-            'messageText' => $message,
+            'messageText' => $data['type'] === 'media'
+                             ? $this->generateAndStoreFile($from, $media['data'], $media['mimetype'])
+                              : $message,
             'messageId' => $data['id']['id'],
             'messageHash' => $hash,
         ];
         WapiMessage::create($logArray);
-
     }
 
-    function sendMessage(Request $request) {
-        $message = $request->message??"Nope";
+    function generateAndStoreFile($from, $media, $mimetype)
+    {
+        $now = Carbon::now("Asia/Kolkata")->timestamp;
+        $data = substr($media, strpos($media, ',') + 1);
+        $file = base64_decode($data);
+        // Use loadHtml for flexibility
+        $filename = $from . "_$now." . mime_content_type($media);
+        $filePath = $mimetype . '/' . $filename;
+        Storage::disk('public')->put($filePath, $file);
+        return Storage::disk('public')->url($filePath);
+    }
+
+    function sendMessage(Request $request)
+    {
+        $message = $request->message ?? "Nope";
         $this->apiService->sendWhatsAppMessage('919524000096@c.us', $message);
     }
-    function test(Request $request) {
+    function test(Request $request)
+    {
         dd($request);
     }
 }
